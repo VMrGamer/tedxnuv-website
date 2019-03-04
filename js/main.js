0386f0e5140f7154fcd6ce310653c52c34b54991
VMrGamer <v.mr.gamer@gmail.com>
2019-03-05 01:19:01 +0530

    Preloader, fixes

--- js/main.js
+++ js/main.js
@@ -8,6 +8,14 @@ jQuery(document).ready(function($) {
 
 	"use strict";
 
+	var browserWindow = $(window);
+
+    // :: 1.0 Preloader Active Code
+    browserWindow.on('load', function () {
+        $('.preloader').fadeOut('slow', function () {
+            $(this).remove();
+        });
+    });
 
 
 	var siteMenuClone = function() {
